4b9e462e15f95f9c902823f19a67c840b3150782
Sarah <70615195+smanter82@users.noreply.github.com>
2020-11-14 18:20:13 -0500

    added links to portfolio projects and updated resume

--- Assets/portfolio.js
+++ Assets/portfolio.js
@@ -1,6 +1,1 @@
-//Add more screenshots to portfolio page
-//Add links to deployed sites
-//Clean up index page and add more info?
-//Make portfolio page responsive.
-
-//Fix ReadMe in Github
+//Add links to Deployed sites
